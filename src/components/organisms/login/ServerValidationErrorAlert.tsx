2257d71ab271249eca17be7b644999c9bcb19128
pedro <pejaraba@yseop.com>
2022-06-28 23:57:05 -0500

    feat(registro): [DOG-11-22] cambiar error de validacion servidor a Card

--- src/components/organisms/login/ServerValidationErrorAlert.tsx
+++ src/components/organisms/login/ServerValidationErrorAlert.tsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { useState } from "react";
-import { Alert } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import styled from "styled-components";
 
 interface ServerValidationErrorAlertProps {
     serverFailedValidations: string[]
 }
 
+const StyledTextDiv = styled("div")`
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: 700;
+`
+
 function ServerValidationErrorAlert(props: ServerValidationErrorAlertProps) {
     return (
-        <Alert variant="danger" >
-            <Alert.Heading>¡Datos inválidos!</Alert.Heading>
-            <ul>
-                {props.serverFailedValidations.map((validation, index) => <li key={index}>{validation}</li>)}
-            </ul>
-        </Alert>
+        <StyledTextDiv className="d-flex">
+            <div className="w-100">
+                <Card text="danger" border="danger" >
+                    <Card.Header ><strong>ERROR</strong></Card.Header>
+                    <Card.Body>
+                        
+                        <Card.Subtitle >
+                            Datos Inválidos:
+                        </Card.Subtitle>
+                        <Card.Text >
+                            <ul>
+                                {props.serverFailedValidations.map((validation, index) => <li key={index}>{validation}</li>)}
+                            </ul>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+        </StyledTextDiv>
+
+
+
     );
 }
 
